1e152a9d11e7a0ab3cd52a93188b04f91ab9a726
Abel Herrera <ahpard97@gmail.com>
2022-10-01 22:49:32 -0500

    add delete buton

--- js/template.js
+++ js/template.js
@@ -13,6 +13,7 @@
 		+		'<div class="view">'
 		+			'<input class="toggle" type="checkbox" {{checked}}>'
 		+			'<label>{{title}}</label>'
+		+			'<button class="destroy"></button>'
 		+		'</div>'
 		+	'</li>';
 	}
